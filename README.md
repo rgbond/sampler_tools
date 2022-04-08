81a349e7e641839e6d80ad0e21b250c72043081d
Bob Bond <rbond@frontier.com>
2022-04-07 21:10:18 -0700

    Added -x option to plot_hs.py

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ data can be plotted via plot_hs.py. The logged hal data and comments can be dump
 via logdb.py
 
 The commands are:
-* log_hs.py log data and run halsampler
-* plot_hs.py plot the data returned from halsampler
-* logdb.py examine and manipulate the data in the database
+* log_hs.py: log data and run halsampler
+* plot_hs.py: plot the data returned from halsampler
+* logdb.py: examine and manipulate the data in the database
 
 To use log_hs.py, first edit log_hs.py and change these python arrays as needed:
 
@@ -19,7 +19,7 @@ To use log_hs.py, first edit log_hs.py and change these python arrays as needed:
 * comments: any comments you want logged in the database for this run.
 * sampler_nets: The nets that the sampler hal utility is saving for halsampler.
 
-Then run the command. It will:
+Then run the log_hs.py command. It will:
 * create the database if it doesn't exist,
 * create a log file for halsampler to use, 
 * log the info in the database, 
@@ -30,8 +30,8 @@ At this point the user would normally run a gcode file that
 triggers sampler. When the run ends, hit return and halsampler will be
 terminated.
 
-plot_hs.py takes optional "-f" command line arguments to specify the fields to plot.
-Valid field names appear in the first line of the log file. The
+plot_hs.py takes optional "-f field" command line arguments to specify the
+fields to plot.  Valid field names appear in the first line of the log file. The
 field name can be extended with a ":n" to scale that field. For example, to see
 "x-vel-cmd" in inches per minute rather than inches per second, use
 "-f x-vel-cmd:60". Plot_hs.py takes a list of one or more log file names. All
@@ -39,6 +39,9 @@ data is displayed on one screen to help show what changed between runs.
 
 This command generated screenshot.png
 * plot_hs.py -f x-vel-cmd -f x-vel-fb 001.out 002.out
+
+plot_hs.py can have a "-x field" argument to specify the field value to use
+as the x axis in the plot.
 
 plot_hs.py will plot all of the fields in the data files if called with no
 "-f" arguments.
--- plot_hs.py
+++ plot_hs.py
@@ -8,19 +8,21 @@
 # 0.000000 0.000000 -0.012800 0.005709 -0.011239 
 # 0.000000 0.000000 -0.012800 0.005709 -0.011239 
 #
-# Usage plot_hs -f col1 -f col2 ... -f coln file1 file2
-#     assumes the columns of data have a label in row 1
-#
-# The colx arguments pick off columns to plot from the file
-# example:
-#
-# echo 'x-pos-cmd x-vel-cmd x-pid-out x-pos-fb x-vel-fb' > run.out
-# halsampler -c 0 >> run.out
-#
-# plot_hs -f x-pos-cmd -f x-pos-fb run.out
+# Usage plot_hs [-x col0] [-f col1] ... [-f coln] file1 file2
 #
 # The program plots all of the fields in the data file if called with
 # no "-f" arguments.
+#
+# The -f colx and -x colx arguments pick off columns to plot from the file
+# example:
+#
+# plot_hs -f x-pos-cmd -f x-pos-fb run.out
+#
+# A "-x" argument indicates a label to be used as the x axis instead of
+# the default line number. For example, the following command will plot the
+# the commanded position against the position feedback:
+#
+# plot_hs -x x-pos-cmd -f x-pos-fb run.out
 #
 # Copyright 2021 Robert Bond
 #
@@ -42,7 +44,7 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
-def process_file(f, fields, scale):
+def process_file(x_name, f, fields, scale):
     n = 0
     lines = f.readlines()
     if len(lines) < 2:
@@ -52,6 +54,16 @@ def process_file(f, fields, scale):
         print("0.000000 0.000000 -0.012800")
         exit(1)
     labels = lines[0].strip().split()
+    if x_name is None:
+        x_index = None
+        xi = 0.0
+    else:
+        if x_name in labels:
+            x_index = labels.index(x_name)
+            x_scale = scale[x_name]
+        else:
+            print(x_name, "not in file header")
+            exit(1)
     if len(fields) == 0:
         fields = labels
         scale = {}
@@ -60,7 +72,7 @@ def process_file(f, fields, scale):
     indicies = []
     lscale = []
     for field in fields:
-        if field not in labels:
+        if not (field in labels):
             print(field, "not in file header")
             exit(1)
     for i, l in enumerate(labels):
@@ -71,14 +83,29 @@ def process_file(f, fields, scale):
             lscale.append(1.0)
     plot_labels = [labels[i] for i in indicies]
     plot_data = []
+    x_values = []
     for line in lines[1:]:
         lf = line.strip().split()
         if lf[0].isalpha():
             print("skipping", lf[0])
             continue
         plot_data.append([float(lf[i]) * lscale[i] for i in indicies])
-    return plot_labels, np.array(plot_data).transpose()
+        if x_index is None:
+            x_values.append(xi)
+            xi += 1.0
+        else:
+            x_values.append(float(lf[x_index]) * x_scale)
+    return plot_labels, x_values, np.array(plot_data).transpose()
         
+def handle_scale_factor(f, scale):
+    if ':' in f:
+        fs = f.split(':')
+        scale[fs[0]] = float(fs[1])
+        return fs[0]
+    else:
+        scale[f] = 1.0
+        return f
+
 class plotter(object):
     def __init__(self, nplots):
         if nplots == 1:
@@ -89,8 +116,7 @@ class plotter(object):
             self.fig, self.axs = plt.subplots(nplots)
         self.pn = 0
 
-    def plot_subplot(self, labels, data, file_name):
-        x_values = np.arange(len(data[0]))
+    def plot_subplot(self, labels, x_values, data, file_name):
         for i, l in enumerate(labels):
             self.axs[self.pn].plot(x_values, data[i], label=l)
         self.axs[self.pn].grid(b=True, which='major', axis='x')
@@ -107,22 +133,20 @@ if __name__ == "__main__":
                     help='Print debug info')
     ap.add_argument('--field', '-f', action='append',
                     help='Field name, optional :scale')
+    ap.add_argument('--x', '-x', help='Select X axis for plot')
     ap.add_argument('files', nargs='+', help='Files to process')
     args = ap.parse_args()
 
     scale = {}
+    if args.x is None:
+        x_name = None
+    else:
+        x_name = handle_scale_factor(args.x, scale)
     fields = []
-    if not (args.field is None):
+    if not args.field is None:
         for f in args.field:
-            if ':' in f:
-                fs = f.split(':')
-                fields.append(fs[0])
-                scale[fs[0]] = float(fs[1])
-            else:
-                fields.append(f)
-                scale[f] = 1.0
-    print(fields)
-
+            f = handle_scale_factor(f, scale)
+            fields.append(f)
     if args.verbose > 0:
         print("fields:", fields)
         print("scale:", scale)
@@ -131,8 +155,8 @@ if __name__ == "__main__":
     for fname in args.files:
         try:
             f = open(fname, 'r')
-            labels, data = process_file(f, fields, scale)
-            plot.plot_subplot(labels, data, fname)
+            labels, x_values, data = process_file(x_name, f, fields, scale)
+            plot.plot_subplot(labels, x_values, data, fname)
             f.close()
         except IOError as e:
             print("Can't open file", fname)
